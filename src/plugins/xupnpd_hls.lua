08a4360ca3e64afebaf2c6880e8744385e5c3ff1
clark15b <clark15b@gmail.com>
2015-10-23 16:46:01 +0300

    HLS

--- src/plugins/xupnpd_hls.lua
+++ src/plugins/xupnpd_hls.lua
@@ -1,30 +1,71 @@
--- Copyright (C) 2011-2013 Anton Burdinuk
+-- Copyright (C) 2011-2015 Anton Burdinuk
 -- clark15b@gmail.com
 -- https://tsdemuxer.googlecode.com/svn/trunk/xupnpd
 
+function hls_get_index(url,t)
+    local pls_data=plugin_download(url)
+
+    if not pls_data then return nil end
+
+    local fseq=tonumber(string.match(pls_data,"#EXT%-X%-MEDIA%-SEQUENCE:(%d+)\r?\n"))
+
+    if not fseq then return nil end
+
+    local seq=fseq
+
+    for d,u in string.gmatch(pls_data,'#EXTINF:(%d+%.?%d-),\r?\n(.-)\r?\n') do
+
+        if string.sub(u,1,7) ~= 'http://' then u=string.match(url,'(.+/).+$')..u end
+
+        t[seq]={ duration=tonumber(d), ['url']=u, ['seq']=seq }
+
+        seq=seq+1
+    end
+
+    return fseq,seq-1
+end
+
 function hls_sendurl(url,range)
 
+    local t={}
+
+    local seq=nil
+    local idx_seq=0
+
     while true do
-        local pls_data=plugin_download(url)
 
-        if not pls_data then break
+        if not seq or seq>=idx_seq then
+            local fseq
 
-        local pls=m3u.parse(pls_data)
+            fseq,idx_seq=hls_get_index(url,t)
 
-        if not pls or pls.size<1 then break
+            if not fseq then break end
 
-        for n,location in ipairs(pls.elements) do
-
-            for i=1,5,1 do
-                rc,location=http.sendurl(location,1)
-
-                if not location then
-                    break
-                else
-                    if cfg.debug>0 then print('Redirect #'..i..' to: '..location) end
-                end
-            end
+            if not seq or fseq>seq then seq=fseq end
         end
+
+        local chunk=t[seq]
+
+        if chunk then
+            print('Chunk '..seq..': '..chunk.url)
+
+            local t1=os.time()
+
+            http.send('\r\n')
+
+            if http.sendurl(chunk.url,0)~=1 then break end
+
+            local delay=chunk.duration-os.difftime(os.time(),t1)-1
+
+            t[seq]=nil
+
+            seq=seq+1
+
+            if delay>0 then util.sleep(delay) end
+        else
+            util.sleep(1)
+        end
+
     end
 
 end
